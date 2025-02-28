2b9d3c1881f6f50fc7ac5b84fa9c376f6363ebff
mahmuhas <169142689+mahmuhas@users.noreply.github.com>
2025-02-28 19:26:10 +0000

    Excercises Week 1 und 2

--- Week_02/exercises/python_linting_exercise.py
+++ Week_02/exercises/python_linting_exercise.py
@@ -1,14 +1,19 @@
-# Import libraries
+"""
+This script demonstrates the creation of a pandas DataFrame
+and prints its type and content.
+"""
+
 import pandas as pd
-import numpy as np
 
 # Initialize data of lists
-data =                pd.DataFrame({'Name': ['tom', 'nick', 'krish', 'jack']          ,
-                     'Age': [20, 21, 19, 18]})
+data = {
+    'Name': ['tom', 'nick', 'krish', 'jack'],
+    'Age': [20, 21, 19, 18]
+}
 
 # Create DataFrame
-df = pd.DataFrame(                  data)       
-print(       type(df)               )
+df = pd.DataFrame(data)
+print(type(df))
 
 # Print the output
 print(df.head())
